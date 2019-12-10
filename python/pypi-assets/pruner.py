7bb4c3884834ae44159cd358c39a720b0623095a
Bruno Rocha <rochacbruno@gmail.com>
2019-12-10 12:00:25 -0200

    Make travis CI happy.
    Looks like new version of pylint is checking for the use of `exit`

--- python/pypi-assets/pruner.py
+++ python/pypi-assets/pruner.py
@@ -4,43 +4,49 @@
 import argparse
 import copy
 import json
+import sys
 
 
 def main():
     """Parse arguments, consume stdin, and produce stdout."""
-    parser = argparse.ArgumentParser(description='Prune PyPI JSON metadata.')
+    parser = argparse.ArgumentParser(description="Prune PyPI JSON metadata.")
     parser.add_argument(
-        'metadata_path',
-        type=argparse.FileType('r'),
-        help=('''\
+        "metadata_path",
+        type=argparse.FileType("r"),
+        help=(
+            """\
             The path to a PyPI JSON metadata file. If -, the file is read from
             stdin.
-            '''),
+            """
+        ),
     )
     parser.add_argument(
-        'distributions',
+        "distributions",
         type=json.loads,
-        help='A JSON list of filenames to search for and keep in the metadata.'
+        help="A JSON list of filenames to search for and keep in the metadata.",
     )
     args = parser.parse_args()
     try:
-        print(prune_metadata(json.load(args.metadata_path), args.distributions))
+        print(
+            prune_metadata(json.load(args.metadata_path), args.distributions)
+        )
     finally:
         args.metadata_path.close()
+
 
 def prune_metadata(old_metadata, available_distributions):
     """Cut unwanted values out of the JSON metadata at ``metadata_path``."""
     # release: e.g. "1.0.1"
     # distributions: e.g. [], or [{egg info}, {wheel info}]
     new_metadata = copy.deepcopy(old_metadata)
-    for release, distributions in old_metadata['releases'].items():
+    for release, distributions in old_metadata["releases"].items():
         for distribution in distributions:
-            if distribution['filename'] not in available_distributions:
-                new_metadata['releases'][release].remove(distribution)
-        if new_metadata['releases'][release] == []:
-            del new_metadata['releases'][release]
+            if distribution["filename"] not in available_distributions:
+                new_metadata["releases"][release].remove(distribution)
+        if new_metadata["releases"][release] == []:
+            del new_metadata["releases"][release]
     return json.dumps(new_metadata, indent=4)
 
 
 if __name__ == "__main__":
-    exit(main())
+    sys.exit(main())
--- python/pypi-assets/urlformatter.py
+++ python/pypi-assets/urlformatter.py
@@ -3,6 +3,7 @@
 """Format urls in PyPI JSON metadata."""
 import argparse
 import json
+import sys
 
 from functools import reduce
 from urllib.parse import urljoin
@@ -10,19 +11,23 @@ from urllib.parse import urljoin
 
 def main():
     """Parse arguments, consume stdin, and produce stdout."""
-    parser = argparse.ArgumentParser(description='Format the URLs in the PyPI JSON metadata.')
-    parser.add_argument(
-        'metadata_path',
-        type=argparse.FileType('r'),
-        help=('''\
-            The path to a PyPI JSON metadata file. If -, the file is read from
-            stdin.
-            '''),
+    parser = argparse.ArgumentParser(
+        description="Format the URLs in the PyPI JSON metadata."
     )
     parser.add_argument(
-        'base_url',
+        "metadata_path",
+        type=argparse.FileType("r"),
+        help=(
+            """\
+            The path to a PyPI JSON metadata file. If -, the file is read from
+            stdin.
+            """
+        ),
+    )
+    parser.add_argument(
+        "base_url",
         type=str,
-        help='base_url at which the fixture packages are located'
+        help="base_url at which the fixture packages are located",
     )
     args = parser.parse_args()
     try:
@@ -30,17 +35,22 @@ def main():
     finally:
         args.metadata_path.close()
 
+
 def format_url(metadata, base_url):
-    """
-    Format the distribution urls in metadata to point to the distributions hosted on base_url
-    """
-    for distributions in metadata['releases'].values():
+    """Format the distribution urls in metadata to point to the distributions
+    hosted on base_url"""
+    for distributions in metadata["releases"].values():
         for distribution in distributions:
-            distribution['url'] = reduce(urljoin, [base_url,
-                                                   'fixtures/python-pypi/packages/',
-                                                   distribution['filename']])
+            distribution["url"] = reduce(
+                urljoin,
+                [
+                    base_url,
+                    "fixtures/python-pypi/packages/",
+                    distribution["filename"],
+                ],
+            )
     return json.dumps(metadata, indent=4)
 
 
 if __name__ == "__main__":
-    exit(main())
+    sys.exit(main())
