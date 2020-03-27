2f93db70d9422aa70d3b7d988aa6b5eaac60efa8
Quirin Pamp <pamp@atix.de>
2020-03-27 16:57:43 +0100

    Also add Ubuntu backports like fixture

---
+++ debian/sign_deb_release.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+RELEASE_FILE="${1}"
+OUTPUT_DIR="$(dirname "${1}")"
+DETACHED_SIGNATURE_PATH="${OUTPUT_DIR}/Release.gpg"
+INLINE_SIGNATURE_PATH="${OUTPUT_DIR}/InRelease"
+
+GPG_KEY_ID="Pulp QE"
+
+COMMON_GPG_OPTS=(
+  --batch
+  --armor
+  --digest-algo
+  SHA256
+)
+
+# Create a detached signature
+/usr/bin/gpg "${COMMON_GPG_OPTS[@]}" \
+  --detach-sign \
+  --output "${DETACHED_SIGNATURE_PATH}" \
+  --local-user "${GPG_KEY_ID}" \
+  "${RELEASE_FILE}"
+
+# Create an inline signature
+/usr/bin/gpg "${COMMON_GPG_OPTS[@]}" \
+  --clearsign \
+  --output "${INLINE_SIGNATURE_PATH}" \
+  --local-user "${GPG_KEY_ID}" \
+  "${RELEASE_FILE}"
