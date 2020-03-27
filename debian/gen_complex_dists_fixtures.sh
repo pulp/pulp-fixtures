8904064cc5525c9b973de754af1e2d6c0a46bb4f
Quirin Pamp <pamp@atix.de>
2020-03-27 16:57:43 +0100

    Also add Ubuntu backports like fixture

--- debian/gen_complex_dists_fixtures.sh
+++ debian/gen_complex_dists_fixtures.sh
@@ -39,6 +39,18 @@ COMMON_REPREPRO_OPTIONS=(
 reprepro "${COMMON_REPREPRO_OPTIONS[@]}" -C asgard includedeb ragnarok/updates asgard/*.deb
 reprepro "${COMMON_REPREPRO_OPTIONS[@]}" -C jotunheimr includedeb ragnarok/updates jotunheimr/*.deb
 
+# Also create the Ubuntu backports like repo fixture:
+reprepro "${COMMON_REPREPRO_OPTIONS[@]}" -C asgard includedeb ragnarok-backports asgard/*.deb
+reprepro "${COMMON_REPREPRO_OPTIONS[@]}" -C jotunheimr includedeb ragnarok-backports jotunheimr/*.deb
+
+RAGNAROK_BACKPORTS_RELEASE_FILE="${TMPDIR}/dists/ragnarok-backports/Release"
+
+# Actually alter the Codename in the release file like Ubuntu does:
+sed -i "s/Codename: ragnarok-backports/Codename: ragnarok/g" "${RAGNAROK_BACKPORTS_RELEASE_FILE}"
+
+# Now sign the Ubuntu like fixtures using an external singing script:
+"${SRCDIR}/sign_deb_release.sh" "${RAGNAROK_BACKPORTS_RELEASE_FILE}"
+
 mkdir -p "${OUTPUTDIR}"
 cp -r --no-preserve=mode --reflink=auto "${TMPDIR}"/dists -t "${OUTPUTDIR}"
 cp -r --no-preserve=mode --reflink=auto "${TMPDIR}"/pool -t "${OUTPUTDIR}"
