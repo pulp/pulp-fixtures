83f60ebec68146763e5b04b7153a7d9eaf71616a
David Davis <daviddavis@redhat.com>
2020-05-28 11:34:00 -0400

    Publish container to docker hub too
    ref #6767

---
+++ .travis/docker-push.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#!/usr/bin/env bash
+
+# docker-push.sh: Push (Upload) image to docker.
+# Image must be already tagged.
+
+# TODO: These are already hardcoded in .travis.yml for the build task
+#
+# Pulp is an organization (not an individual user account) on Docker:
+# https://docker.io/organization/pulp
+# For test publishes, one can override this to any org or user.
+DOCKER_PROJECT_NAME=${DOCKER_USER_OR_ORG_NAME:-pulp}
+# The image name, AKA the Docker repo
+DOCKER_REPO_NAME=${DOCKER_USER_OR_ORG_NAME:-pulp-fixtures}
+# The image tag
+IMAGE_TAG=${DOCKER_USER_OR_ORG_NAME:-latest}
+
+echo "$DOCKER_BOT_PASSWORD" | docker login -u "$DOCKER_BOT_USERNAME" --password-stdin quay.io
+docker push "$DOCKER_PROJECT_NAME/$DOCKER_REPO_NAME:$IMAGE_TAG"
