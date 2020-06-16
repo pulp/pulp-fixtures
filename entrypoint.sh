a24e40bbc8422447015944328919629d49870216
David Davis <daviddavis@redhat.com>
2020-06-16 16:32:55 -0400

    Update BASE_URL for json files too

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 # inject BASE_URL at runtime
-find /usr/share/nginx/html \( -name "*mirrorlist*" -o -name "*.html" \) \
+find /usr/share/nginx/html \( -name "*mirrorlist*" -o -name "*.html" -o -name "*.json" \) \
   -exec sed -i -e "s%BASE_URL%$BASE_URL%g" {} +
 
 nginx -g 'daemon off;'
