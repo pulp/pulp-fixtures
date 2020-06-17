c25efb875ea81cc3b7e0d446372ffe496b65d030
David Davis <daviddavis@redhat.com>
2020-06-17 08:48:42 -0400

    Fix problem generating fixture that uses urljoin()

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ set -e
 
 # inject BASE_URL at runtime
 find /usr/share/nginx/html \( -name "*mirrorlist*" -o -name "*.html" -o -name "*.json" \) \
-  -exec sed -i -e "s%BASE_URL%$BASE_URL%g" {} +
+  -exec sed -i -e "s%http://BASE_URL%$BASE_URL%g" {} +
 
 nginx -g 'daemon off;'
