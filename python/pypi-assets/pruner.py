596bf9437b3e8767b9721fdf336e8feaa877861f
Gerrod Ubben <gerrodubben@gmail.com>
2023-10-28 16:31:15 -0400

    Implement PyPI version json API & added two packages for dependency syncing
    [noissue]

--- python/pypi-assets/pruner.py
+++ python/pypi-assets/pruner.py
@@ -26,32 +26,33 @@ def main():
         help="A JSON list of filenames to search for and keep in the metadata.",
     )
     parser.add_argument(
-        "version",
-        type=json.loads,
-        help="A str of the latest version we'll be using for each package"
+        "version_path",
+        type=argparse.FileType("r"),
+        help="The path to the PyPI version JSON metadata file for the latest file"
     )
     args = parser.parse_args()
     try:
-        print(prune_metadata(json.load(args.metadata_path), args.distributions, args.version))
+        print(
+            prune_metadata(
+                json.load(args.metadata_path), args.distributions, json.load(args.version_path)
+            )
+        )
     finally:
         args.metadata_path.close()
 
 
-def prune_metadata(old_metadata, available_distributions, latest_version):
+def prune_metadata(old_metadata, available_distributions, latest_version_metadata):
     """Cut unwanted values out of the JSON metadata at ``metadata_path``."""
     # release: e.g. "1.0.1"
     # distributions: e.g. [], or [{egg info}, {wheel info}]
-    new_metadata = copy.deepcopy(old_metadata)
-    new_metadata["info"]["version"] = latest_version
+    latest_version_metadata["releases"] = copy.deepcopy(old_metadata["releases"])
     for release, distributions in old_metadata["releases"].items():
         for distribution in distributions:
             if distribution["filename"] not in available_distributions:
-                new_metadata["releases"][release].remove(distribution)
-        if new_metadata["releases"][release] == []:
-            del new_metadata["releases"][release]
-        if release == latest_version:
-            new_metadata["urls"] = new_metadata["releases"][release]
-    return json.dumps(new_metadata, indent=4)
+                latest_version_metadata["releases"][release].remove(distribution)
+        if latest_version_metadata["releases"][release] == []:
+            del latest_version_metadata["releases"][release]
+    return json.dumps(latest_version_metadata, indent=4)
 
 
 if __name__ == "__main__":
